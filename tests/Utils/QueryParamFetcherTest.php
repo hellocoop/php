4544c1487183d37be4aa356e8c0afb2a2a3c4600
Unnikrishnan <unnikrishnanadoor@gmail.com>
2024-12-04 21:07:18 +0700

    Added unit tests for QueryParamFetcher and RequestParamFetcher classes. Covered methods for parameter fetching from GET, POST, and headers, including default values and multiple keys. Ensured robust testing for both individual and batch fetch operations.

---
+++ tests/Utils/QueryParamFetcherTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace HelloCoop\Tests\Utils;
+
+use PHPUnit\Framework\TestCase;
+use HelloCoop\Utils\QueryParamFetcher;
+
+class QueryParamFetcherTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        // Backup the original $_GET superglobal
+        $this->originalGet = $_GET;
+    }
+
+    protected function tearDown(): void
+    {
+        // Restore the original $_GET superglobal
+        $_GET = $this->originalGet;
+    }
+
+    public function testFetchWithExistingKeys()
+    {
+        $_GET = [
+            'key1' => 'value1',
+            'key2' => 'value2',
+        ];
+
+        $result = QueryParamFetcher::fetch(['key1', 'key2']);
+
+        $this->assertEquals([
+            'key1' => 'value1',
+            'key2' => 'value2',
+        ], $result);
+    }
+
+    public function testFetchWithNonExistentKeys()
+    {
+        $_GET = [
+            'key1' => 'value1',
+        ];
+
+        $result = QueryParamFetcher::fetch(['key2', 'key3']);
+
+        $this->assertEquals([
+            'key2' => null,
+            'key3' => null,
+        ], $result);
+    }
+
+    public function testFetchWithMixedKeys()
+    {
+        $_GET = [
+            'key1' => 'value1',
+            'key3' => 'value3',
+        ];
+
+        $result = QueryParamFetcher::fetch(['key1', 'key2', 'key3']);
+
+        $this->assertEquals([
+            'key1' => 'value1',
+            'key2' => null,
+            'key3' => 'value3',
+        ], $result);
+    }
+}
