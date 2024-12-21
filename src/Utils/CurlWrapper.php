a2588027acae415f48c661c867d79eb73b686124
Santhosh Raju <santhosh.raju@gmail.com>
2024-12-21 10:32:35 +0100

    Fix lint in Utils.

--- src/Utils/CurlWrapper.php
+++ src/Utils/CurlWrapper.php
@@ -2,35 +2,63 @@
 
 namespace HelloCoop\Utils;
 
+// XXX: phpstan ignores can be removed when 7.x support is dropped in library.
 class CurlWrapper
 {
+    /**
+     * @param string $url
+     * @return false|resource
+     */
     public function init(string $url)
     {
         return curl_init($url);
     }
 
+    /**
+     * @param resource $ch
+     * @param int $option
+     * @param mixed $value
+     * @return bool
+     */
     public function setOpt($ch, int $option, $value): bool
     {
-        return curl_setopt($ch, $option, $value);
+        return curl_setopt($ch, $option, $value); // @phpstan-ignore argument.type
     }
 
+    /**
+     * @param resource $ch
+     * @return bool|string
+     */
     public function exec($ch)
     {
-        return curl_exec($ch);
+        return curl_exec($ch); // @phpstan-ignore argument.type
     }
 
+    /**
+     * @param resource $ch
+     * @param int $option
+     * @return mixed
+     */
     public function getInfo($ch, int $option)
     {
-        return curl_getinfo($ch, $option);
+        return curl_getinfo($ch, $option); // @phpstan-ignore argument.type
     }
 
+    /**
+     * @param resource $ch
+     * @return void
+     */
     public function close($ch): void
     {
-        curl_close($ch);
+        curl_close($ch); // @phpstan-ignore argument.type
     }
 
+    /**
+     * @param resource $ch
+     * @return string
+     */
     public function error($ch): string
     {
-        return curl_error($ch);
+        return curl_error($ch); // @phpstan-ignore argument.type
     }
 }
--- src/Utils/HelloString.php
+++ src/Utils/HelloString.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace HelloCoop\Tests\Utils;
+namespace HelloCoop\Utils;
 
 class HelloString
 {
-    public static function startsWith($haystack, $needle)
+    public static function startsWith(string $haystack, string $needle): bool
     {
         return substr_compare($haystack, $needle, 0, strlen($needle)) === 0;
     }
-    public static function endsWith($haystack, $needle)
+    public static function endsWith(string $haystack, string $needle): bool
     {
         return substr_compare($haystack, $needle, -strlen($needle)) === 0;
     }
