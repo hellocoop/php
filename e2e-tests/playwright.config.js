8286f0e980804b8f8d50aff10a94be0b33b79610
Santhosh Raju <santhosh.raju@gmail.com>
2025-08-21 11:10:25 +0200

    Add e2e tests to GHA.

--- e2e-tests/playwright.config.js
+++ e2e-tests/playwright.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:8000',
+    // baseURL: 'http://localhost:8000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -45,14 +45,14 @@ export default defineConfig({
   webServer: [
     {
        command: 'php -S localhost:8000',
-       url: 'http://127.0.0.1:8000',
+       url: 'http://localhost:8000',
        // stdout: 'pipe',
        timeout: 10000,
        reuseExistingServer: !process.env.CI,
     },
     {
        command: 'npx @hellocoop/mockin',
-       url: 'http://127.0.0.1:3333/version',
+       url: 'http://localhost:3333/version',
        // stdout: 'pipe',
        timeout: 10000,
        reuseExistingServer: !process.env.CI,
